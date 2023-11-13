4e29d7e4d468960d407fdf8fa0c79927dfbf4d4a
32602100045_Fatwaakbarjiwani <fatwaakbar2003@gmail.com>
2023-11-13 20:05:35 +0700

    env_stagging

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import rootReducers from "./reducers";
 import thunk from "redux-thunk";
 
+
+
 // Create the store or temporary state database
 export default configureStore({
   reducer: rootReducers,
